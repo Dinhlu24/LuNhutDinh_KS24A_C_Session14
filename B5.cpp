20f80da66cfcfa760c509f0ed6efdc59664fad9c
Dinhlu24 <dinhlunhut@gmail.com>
2025-01-02 08:29:52 +0700

    Update B5.cpp

--- B5.cpp
+++ B5.cpp
@@ -3,9 +3,11 @@
 
 int main(){
 	char str[100];	printf("Moi nhap vao mot chuoi bat ki: ");	gets(str);
-	int cnt=1;
+	int cnt=0;
+	
 	for(int i=0;i<strlen(str);i++){
-		if(str[i] == ' ')	cnt++;
+		if(str[i] == ' ' && str[i+1] != ' ' && i!=strlen(str)-1 || (i==0 && str[i] != ' '))	cnt++;
 	}
+	
 	printf("Tat ca so tu trong mang la: %d", cnt);
 }
